4df3886486636ecc55c94c2285c15a668bebebe2
Murodbek Raximjonov <murodbekraximjonov@Murodbeks-MacBook-Air.local>
2023-03-15 17:44:03 +0500

    navbar_style

---
+++ src/components/navbar/navbar.component.tsx
@@ -0,0 +1,5 @@
+import { NavbarComponent } from './navbar.style'
+
+export const Navbar = () => {
+    return <NavbarComponent>navbar</NavbarComponent>
+}
---
+++ src/components/navbar/navbar.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const NavbarComponent = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    background: white;
+    box-shadow: 0 0 13px -3px #333;
+`
